0eaa0ce5398501052d7d0c493c4b39a273a4ebb5
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-06-04 13:38:27 +0200

    feat(frontend): add vue-router with login route and integrate in app (#26)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,11 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
+import router from "./router/index.ts";
 import "./style.css";
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(router);
 app.mount("#app");
