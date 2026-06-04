1f045fb05d52e91a8fa5bdac396805a1700634e5
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-06-04 13:35:13 +0200

    feat(frontend): add vue-router with login route and integrate in app

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Define routes config
+const routes = [
+	{
+		path: "/login",
+		name: "Login",
+		// Lazy-loading : load only when the route is called => more efficient, don't load all at once
+		component: () => import("../views/LoginPage.vue"),
+	},
+];
+
+// createRouter : create a router instance
+// createWebHistory : use the html5 history mode => /login instead of #/login
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+export default router;
