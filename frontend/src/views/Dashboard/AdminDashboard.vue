a712b7ded9c525776965551aa9005df501c59196
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-06-15 17:05:29 +0200

    feat: add authentication guard and post-login dashboard routing
    Fixed #51

---
+++ frontend/src/views/Dashboard/AdminDashboard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="h-screen flex justify-center items-center">
+        <h1 class="font-heading">Admin Dashboard</h1>
+    </div>
+</template>
