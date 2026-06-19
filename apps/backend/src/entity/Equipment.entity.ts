b5b6a8aa7367aeb34393e7f158b22a30fcef5fb9
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-06-19 22:48:49 +0200

    feat: add Swagger documentation for Equipment entity and classrooms/{classroomId}/equipments endpoint
    Fixed #161

--- apps/backend/src/entity/Equipment.entity.ts
+++ apps/backend/src/entity/Equipment.entity.ts
@@ -1,5 +1,19 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Equipment:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: L'identifiant de l'équipement
+ *         name:
+ *           type: string
+ *           description: Le nom de l'équipement
+ */
 @Entity({ name: "equipment" })
 export class Equipment {
 	@PrimaryGeneratedColumn({ type: "bigint" })
