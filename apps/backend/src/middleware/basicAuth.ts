e081846d08d66b92e342e76bcc97fe82ec61aef8
Pierddev <139257364+Pierddev@users.noreply.github.com>
2026-06-24 17:18:30 +0200

    biome fixes

--- apps/backend/src/middleware/basicAuth.ts
+++ apps/backend/src/middleware/basicAuth.ts
@@ -9,14 +9,14 @@ export function basicAuth(req: Request, res: Response, next: NextFunction) {
 		if (base64) {
 			const decodeAuth = atob(base64);
 
-            const [user, pass] = decodeAuth.split(":")
+			const [user, pass] = decodeAuth.split(":");
 
-            // Temporaire, pour test en attendant de configurer pour fonctionner avec jwt
-            if (user === "admin" && pass === "Admin123!") {
-                return next();
-            }
+			// Temporaire, pour test en attendant de configurer pour fonctionner avec jwt
+			if (user === "admin" && pass === "Admin123!") {
+				return next();
+			}
 		}
 	}
 
-	return res.status(401).json({error: "Invalid credentials"});
+	return res.status(401).json({ error: "Invalid credentials" });
 }
