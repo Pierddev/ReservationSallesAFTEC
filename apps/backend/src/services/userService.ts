2d0c06a4b7b46a77235d83d4eaf99fa1fcaae1c9
Pierre DUAULT <pierre@macbook-pro-de-pierre-2.home>
2026-07-07 20:40:25 +0200

    Biome fixes

--- apps/backend/src/services/userService.ts
+++ apps/backend/src/services/userService.ts
@@ -1,9 +1,9 @@
-import { AppDataSource } from "../database/data-source.js"
-import { User } from "../entity/User.entity.js"
+import { AppDataSource } from "../database/data-source.js";
+import { User } from "../entity/User.entity.js";
 
 export class UserService {
-    async getAll(): Promise<User[]> {
-        const userRepository = AppDataSource.getRepository(User);
-        return await userRepository.find();
-    }
+	async getAll(): Promise<User[]> {
+		const userRepository = AppDataSource.getRepository(User);
+		return await userRepository.find();
+	}
 }
